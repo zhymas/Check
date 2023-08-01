635b237030c0fb30b7fab6bf0743c23ebe1c81b7
dima <shvachko319@gmail.com>
2023-08-02 02:02:10 +0300

    fix bugs

--- task/printer/utils.py
+++ task/printer/utils.py
@@ -3,9 +3,10 @@ import requests
 import base64
 from .models import Check
 from django.template.loader import render_to_string
-from task.settings import app as celery_app
+# from task.settings import app as celery_app
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+
 
 def create_html_template(check_id, check_type, context):
     
@@ -15,14 +16,13 @@ def create_html_template(check_id, check_type, context):
         f.write(html)
     return path
 
-@celery_app.task
+
 def html_to_pdf(context):
     path = create_html_template(context.get('check_id'), context.get('check_type'), context)
     with open(path, 'r') as html_file:
         html_contents = html_file.read()
 
     encoded_html = base64.b64encode(html_contents.encode()).decode()
-    
 
     url = 'http://localhost:8080'
     data = {
@@ -40,11 +40,9 @@ def html_to_pdf(context):
     else:
         print(f'Помилка під час створення PDF: {response.status_code} - {response.text}')
 
-    
     with open(f'printer/media/pdf/{pdf_file_name}', 'rb') as f:
         file_data = f.read()
         context.get('check_obj').pdf_file.save(f'printer/media/pdf/{pdf_file_name}', f)
-
 
 
 def print_check(checks):
